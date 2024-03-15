91952139aa88ec89438ecccdaf54d8cd0f2971bc
Sérgio Nunes <sagnunes@gmail.com>
2024-03-15 16:37:16 +0000

    ...

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rounded-lg shadow-lg w-[264px] h-[336px]">
+    <picture>
+      <img src="https://flagcdn.com/de.svg" alt="" class="rounded-t-lg">
+    </picture>
+    <div class="p-6">
+      <h2 class="font-nunito font-extrabold text-xl leading-[26px]">Germany</h2>
+      <ul>
+        <li>Population:</li>
+        <li>Region:</li>
+        <li>Capital:</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CountryCard'
+}
+</script>
+<style>
+select {
+  @apply mt-10 shadow-lg px-6 py-4 rounded font-nunito text-xs leading-5 text-[#111517]
+}
+
+ul {
+  @apply font-nunito font-semibold text-sm leading-4 space-y-2 mt-4
+}
+</style>
---
+++ src/components/FilterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <select name="" id="">
+    <option selected>Filter by Region</option>
+    <option value="">Africa</option>
+    <option value="">America</option>
+    <option value="">Asia</option>
+    <option value="">Europe</option>
+    <option value="">Oceania</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'FilterComponent'
+}
+</script>
+<style>
+select {
+  @apply mt-10 shadow-lg px-6 py-4 rounded font-nunito text-xs leading-5 text-[#111517]
+}
+</style>
